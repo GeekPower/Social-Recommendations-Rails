ea9debafb9c69ad373f634ca28ddeb249d89fe4f
Vlad Voicu <vladvoic@gmail.com>
2011-04-16 15:04:27 +0300

    added lord of the ring example

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
 	def index
+		@my_movie = Movie.where("titlu = ?", "Lord of")[0]
+
+		#@my_movie.titlu = "Lord of the Rings"
+		#@my_movie.link = "http://imbd.com"
+		#if @my_movie.save
+		#else
+		#end
 	end
 end
