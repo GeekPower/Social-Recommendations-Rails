942ee39ca08a0cc94e40a3ed352ce0c822cc72da
Gabriel Voicu <gabriel@Gabriel-Voicus-MacBook-Pro.local>
2011-04-16 20:23:28 +0300

    1) Fixed the Facebook authentification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
+#	before_filter :authenticate_user!
 	def index
 		@my_movie = Movie.where("titlu = ?", "Lord of")[0]
+		
 
 		#@my_movie.titlu = "Lord of the Rings"
 		#@my_movie.link = "http://imbd.com"
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,26 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     # Get the user email info from Facebook for sign up
     # You'll have to figure this part out from the json you get back
-    data = ActiveSupport::JSON.decode(access_token)
 
-    if user = User.find_by_email(data["email"])
+
+
+    #data = ActiveSupport::JSON.decode(access_token.to_s)
+
+    if user = User.find_by_email(access_token["user_info"]["email"])
       user
     else
       # Create an user with a stub password.
-      User.create!(:name => data["name"], :email => data["email"], :password => Devise.friendly_token)
+      User.create!(:name => access_token["user_info"]["name"], :email => access_token["user_info"]["email"], :password => Devise.friendly_token)
     end
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["user_hash"]
-        user.email = data["email"]
-      end
-    end
-  end
+
+
+  #def self.new_with_session(params, session)
+  #  super.tap do |user|
+  #    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["user_hash"]
+  #      user.email = data["email"]
+  #    end
+  #  end
+  #end
 
   end
 end
